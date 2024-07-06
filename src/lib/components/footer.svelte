1dea537b4247939342943ced61715087383157e5
caiofariaas <caiofarias077@gmail.com>
2024-07-06 14:38:41 -0300

    feito componente de footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,68 @@
+<script>
+  </script>
+  
+ 
+  <div class="w-full">
+    <div class="flex flex-col bg-black w-full p-10 items-center">
+      <div class="flex flex-col lg:flex-row w-full text-white">
+        <div class="flex flex-col lg:flex-row w-full justify-center lg:justify-between lg:mx-16 items-center">
+          <img src="/static/img/img.png" alt="" class="w-[200px] h-[200px] lg:w-[285px] lg:h-[285px]" />
+          <div class="text-center lg:text-left">
+            <span class="text-[30px] lg:text-[47px] font-bold">MÍDIAS</span>
+            <div class="flex flex-col gap-2 items-center lg:items-start mt-4">
+              <a href="https://www.instagram.com/_dlvendas" target="_blank" rel="noopener noreferrer" class="flex gap-2 items-center">
+                <img src="/static/img/insta.png" alt="Instagram" class="w-7 h-7" />
+                <span>@_dlvendas</span>
+              </a>
+              <a href="https://www.instagram.com/maurilio.st" target="_blank" rel="noopener noreferrer" class="flex gap-2 items-center">
+                <img src="/static/img/insta.png" alt="Instagram" class="w-7 h-7" />
+                <span>@maurilio.st</span>
+              </a>
+              <a href="https://www.instagram.com/dlimports_reserva" target="_blank" rel="noopener noreferrer" class="flex gap-2 items-center">
+                <img src="/static/img/insta.png" alt="Instagram" class="w-7 h-7" />
+                <span>dlimports_reserva</span>
+              </a>
+              <div class="flex gap-2 items-center mt-4">
+                <img src="/static/img/whatsapp.png" alt="WhatsApp" class="w-7 h-7" />
+                <span>(11) 9 8909-8443</span>
+              </div>
+            </div>
+            <div class="text-white mt-10">
+              <span class="text-[30px] lg:text-[40px] font-bold">LOCALIZAÇÃO</span>
+              <div class="flex gap-3 items-center mt-2">
+                <img src="/static/img/pin.png" alt="Localização" class="w-7 h-7" />
+                <span>R. Glauco Antônio Galli, 423 - Jardim Palmira</span>
+              </div>
+            </div>
+          </div>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3660.7913506708287!2d-46.56456772437722!3d-23.43190317888898!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cef45c89d58edf%3A0xd5e41a005be7910f!2sR.%20Glauco%20Ant%C3%B4nio%20Galli%2C%20423%20-%20Jardim%20Palmira%2C%20Guarulhos%20-%20SP%2C%2007076-010!5e0!3m2!1spt-BR!2sbr!4v1719938412884!5m2!1spt-BR!2sbr"
+            width="100%"
+            height="300"
+            style="border: 0"
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+            class="mt-10 lg:mt-0 lg:w-[400px]"
+          />
+        </div>
+      </div>
+      <div class="flex w-[70vw] mt-10 items-center flex-wrap text-white text-[20px] gap-x-20 gap-y-4 justify-center">
+        <span class="cursor-pointer">HOME</span>
+        <span class="cursor-pointer">AVALIAÇÕES</span>
+        <span class="cursor-pointer">SOBRE NÓS</span>
+        <span class="cursor-pointer">CONTATO</span>
+        <span class="cursor-pointer">TÊNIS</span>
+        <span class="cursor-pointer">BONÉS</span>
+        <span class="cursor-pointer">POLOS</span>
+        <span class="cursor-pointer">BERMUDAS</span>
+        <span class="cursor-pointer">BLUSAS DE FRIO</span>
+        <span class="cursor-pointer">CONJUNTOS</span>
+        <span class="cursor-pointer">CALÇADOS</span>
+      </div>
+    </div>
+    <div class="flex h-16 w-full bg-[#FFBF00] items-center justify-center gap-28 text-black font-[700]">
+      <span>©2024 DL Imports</span>
+      <span>Desenvolvido por D&B Softwares - Todos os direitos reservados</span>
+    </div>
+  </div>
+  
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,13 @@
 <script>
   import CardDestaque from "$components/CardDestaque.svelte";
-import Destaques from "$components/Destaques.svelte";
+  import Destaques from "$components/Destaques.svelte";
+    import Footer from "$components/footer.svelte";
   import Novidades from "$components/Novidades.svelte";
-import SliderTopo from "$components/SliderTopo.svelte";
-
+  import SliderTopo from "$components/SliderTopo.svelte";
 </script>
-<SliderTopo/>
-<Destaques/>
-<CardDestaque/>
-<Novidades/>
+
+<SliderTopo />
+<Destaques />
+<CardDestaque />
+<Novidades />
+<Footer/>
