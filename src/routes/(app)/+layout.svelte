ea1f9eb53e0b9561518e71c2c056e2b174c542dc
flb1ca <flb1ca@bosch.com>
2024-07-08 09:19:01 -0300

    add footer

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import Footer from "$components/footer.svelte";
+  import Footer from "$components/Footer.svelte";
 import NavBar from "$lib/components/NavBar.svelte";
 </script>
 
