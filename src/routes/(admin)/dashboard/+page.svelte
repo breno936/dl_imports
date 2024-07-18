81394a852b9f80f57f72d0be120c461bc6f9047e
flb1ca <flb1ca@bosch.com>
2024-07-18 12:24:50 -0300

    change dashboard design

--- src/routes/(admin)/dashboard/+page.svelte
+++ src/routes/(admin)/dashboard/+page.svelte
@@ -46,9 +46,10 @@
 
 </script>
 
-<div class="stats shadow flex w-max h-max ml-auto mr-auto relative">
-  
-    <div class="stat">
+<div class="stats flex flex-col justify-center items-center w-max h-max ml-auto mr-auto relative">
+  <h1>BEM VINDO MAURILIO</h1>
+  <h2>Navegue pelo painel de administração</h2>
+    <!-- <div class="stat">
       <div class="stat-figure text-secondary">
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
       </div>
@@ -92,7 +93,7 @@
       <div class="stat-value">{hoursClosed}</div>
       <div class="stat-desc">↘︎ 90 (14%)</div>
     </div>
-    
+     -->
   </div>
 
 
@@ -100,5 +101,9 @@
   <style>
     .stats{
         top:45vh;
+    }
+    h1{
+      font-family: Roboto-Black;
+      font-size: 50px;
     }
   </style>
